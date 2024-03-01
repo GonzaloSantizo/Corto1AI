1d99d2eb0cb12c87797d2f9f795aaad2426daa13
GonzaloSantizo <san21504@uvg.edu.gt>
2024-02-29 18:10:02 -0600

    final changes

--- FrozenLake.py
+++ FrozenLake.py
@@ -1,38 +1,69 @@
-
 import gymnasium as gym
 import numpy as np
 import matplotlib.pyplot as plt
-import json
-
-def softmax(q_values, tau=1.0):
-    """Compute softmax probabilities from q_values."""
-    preferences = q_values / tau
-    max_preference = np.max(preferences, axis=1, keepdims=True)
-    exp_preferences = np.exp(preferences - max_preference)
-    sum_exp_preferences = np.sum(exp_preferences, axis=1, keepdims=True)
-    action_probs = exp_preferences / sum_exp_preferences
-    return action_probs
-
-def choose_action_softmax(state, q, tau=1.0):
- 
-    action_probs = softmax(q[state].reshape(1, -1), tau)
-    action = np.random.choice(np.arange(q.shape[1]), p=action_probs.ravel())
-    return action
+import pickle
 
 def run(episodes, is_training=True, render=False):
 
     env = gym.make('FrozenLake-v1', map_name="8x8", is_slippery=True, render_mode='human' if render else None)
 
-    if is_training:
-        q = np.zeros((env.observation_space.n, env.action_space.n))
+    if(is_training):
+        q = np.zeros((env.observation_space.n, env.action_space.n)) # init a 64 x 4 array
     else:
-        with open('frozen_lake8x8.json', 'r') as f:
-            q = np.array(json.load(f))
+        f = open('frozen_lake8x8.pkl', 'rb')
+        q = pickle.load(f)
+        f.close()
 
-    learning_rate_a = 0.9
-    discount_factor_g = 0.9
-    tau = 1.0  # Temperature parameter for softmax
-    tau_decay = 0.99
-    rng = np.random.default_rng()
+    learning_rate_a = 0.9 # alpha or learning rate
+    discount_factor_g = 0.9 # gamma or discount rate. Near 0: more weight/reward placed on immediate state. Near 1: more on future state.
+    epsilon = 1         # 1 = 100% random actions
+    epsilon_decay_rate = 0.0001        # epsilon decay rate. 1/0.0001 = 10,000
+    rng = np.random.default_rng()   # random number generator
 
     rewards_per_episode = np.zeros(episodes)
+
+    for i in range(episodes):
+        state = env.reset()[0]  # states: 0 to 63, 0=top left corner,63=bottom right corner
+        terminated = False      # True when fall in hole or reached goal
+        truncated = False       # True when actions > 200
+
+        while(not terminated and not truncated):
+            if is_training and rng.random() < epsilon:
+                action = env.action_space.sample() # actions: 0=left,1=down,2=right,3=up
+            else:
+                action = np.argmax(q[state,:])
+
+            new_state,reward,terminated,truncated,_ = env.step(action)
+
+            if is_training:
+                q[state,action] = q[state,action] + learning_rate_a * (
+                    reward + discount_factor_g * np.max(q[new_state,:]) - q[state,action]
+                )
+
+            state = new_state
+
+        epsilon = max(epsilon - epsilon_decay_rate, 0)
+
+        if(epsilon==0):
+            learning_rate_a = 0.0001
+
+        if reward == 1:
+            rewards_per_episode[i] = 1
+
+    env.close()
+
+    sum_rewards = np.zeros(episodes)
+    for t in range(episodes):
+        sum_rewards[t] = np.sum(rewards_per_episode[max(0, t-100):(t+1)])
+    plt.plot(sum_rewards)
+    plt.savefig('frozen_lake8x8.png')
+
+    if is_training:
+        f = open("frozen_lake8x8.pkl","wb")
+        pickle.dump(q, f)
+        f.close()
+
+if __name__ == '__main__':
+    # run(15000)
+
+    run(1000, is_training=True, render=True)
